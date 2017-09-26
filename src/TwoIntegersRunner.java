8e16898bcc4ce2f5b2f9555e942cc13ffa856686
ntkelsh <ntkelsh@gmail.com>
2017-09-26 16:43:20 -0700

    Renamed

--- src/TwoIntegersRunner.java
+++ src/TwoIntegersRunner.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ArithmeticRunner {
+public class TwoIntegersRunner {
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
@@ -7,7 +7,7 @@ public class ArithmeticRunner {
 		int n1 = sc.nextInt();
 		System.out.println("Enter another number: ");
 		int n2 = sc.nextInt();
-		Arithmetic object = new Arithmetic(n1, n2);
+		TwoIntegers object = new TwoIntegers(n1, n2);
 		System.out.println(object.arithmetic());
 		System.out.printf("%s is bigger", object.compareIntegers());
 		System.out.println("\nThe first number is even: " + object.oddOrEven());
