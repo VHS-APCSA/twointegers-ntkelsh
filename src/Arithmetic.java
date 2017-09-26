ad1be34ca1d706f6c5f599ff8844eb3e6c237da4
ntkelsh <ntkelsh@gmail.com>
2017-09-26 16:42:24 -0700

    Changed class name and constructors

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,14 +1,14 @@
 
-public class Arithmetic
+public class TwoIntegers
 {
 	private int num1;
 	private int num2;
-	public Arithmetic()
+	public TwoIntegers()
 	{
 		num1 = 0;
 		num2 = 0;
 	}
-	public Arithmetic(int num1, int num2)
+	public TwoIntegers(int num1, int num2)
 	{
 		this.num1 = num1;
 		this.num2 = num2;
